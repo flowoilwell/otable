6e6d376a99c791a04e4d3db1f3e469d39aceafd3
Benji York <benji@benjiyork.com>
2022-06-16 12:26:38 -0400

    explicitly state that these names are exported
    Otherwise, mypy will complain when users of the library import these.

--- src/otable/__init__.py
+++ src/otable/__init__.py
@@ -1,5 +1,7 @@
 """Table data types backed by objects."""
 
+__all__ = ('OTable', 'OColumn', 'ORow')
+
 from .ocolumn import OColumn  # noqa
 from .orow import ORow  # noqa
 from .otable import OTable  # noqa
